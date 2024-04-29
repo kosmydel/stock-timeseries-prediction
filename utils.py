9a27009d1e7ad4bdf421be9dba63d1e3e3cb27da
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2024-04-29 15:38:52 +0200

    improve utils

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from darts.metrics import mape, mse, rmse, mae
 import json
 from tqdm.notebook import tqdm
-from typing import List
+from typing import Callable, List
 from darts.models.forecasting.forecasting_model import ForecastingModel
 from darts import TimeSeries
 import matplotlib.pyplot as plt
@@ -33,8 +33,57 @@ def plot_forecast(series, forecast, title):
     plt.legend()
     plt.show()
 
-def run_experiment():
-    print('Running experiment')
+
+class Dataset:
+    def __init__(self, series: TimeSeries, name: str):
+        self.series = series
+        self.name = name
+
+        self.preprocess()
+
+    def preprocess(self):
+        pass
+
+
+class TimeseriesExperiment:
+    # TODO: Add metric support
+    def __init__(self, model: ForecastingModel, dataset: Dataset, parameters: dict, forecast_horizon: int = 3):
+        self.model = model
+        self.dataset = dataset
+        self.parameters = parameters
+        self.forecast_horizon = forecast_horizon
+
+    def load_or_find_parameters(self):
+        model_name = f'{self.dataset.name}_{self.model.__class__.__name__}_{self.forecast_horizon}.json'
+        model = load_model(model_name)
+        if model is None:
+            self.find_parameters()
+            save_model(model_name, self.model)
+        else:
+            self.model = model
+
+    def find_parameters(self):
+        model, parameters, metric = self.model.gridsearch(self.parameters, self.dataset.series, verbose=True)
+
+        self.model = model
+        print('Best parameters:', parameters, 'Metric:', metric)
+
+    def run(self):
+        self.load_or_find_parameters()
+
+        result = self.model.historical_forecasts(self.dataset, forecast_horizon=self.forecast_horizon)
+
+        metrics = calculate_metrics(self.dataset, result)
+        metrics['model'] = model.__class__.__name__
+        metrics['forecast_horizon'] = self.forecast_horizon
+        metrics['dataset'] = self.dataset
+        metrics['experiment_time'] = time.time()
+
+        file_name = f'{RESULTS_PATH}{self.dataset}_{model.__class__.__name__}_{self.forecast_horizon}.json'
+
+        with open(file_name, 'w') as f:
+            json.dump(metrics, f)
+
 
 
 def backtest(models: List[ForecastingModel], series: TimeSeries, dataset: str, forecast_horizon: int = 3):
